637c920876980fc51d3ccbab5da2a32e1bd6d314
mateusz-ptak <mptak1709@gmail.com>
2023-02-11 23:50:32 +0100

    created currency rate webscrapping app

---
+++ app.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.mbank.pl/serwis-ekonomiczny/kursy-walut/"
+resp = requests.get(url)
+soup = BeautifulSoup(resp.content, "html.parser")
+
+# tag informs from which day the exchange rate is
+tag = soup.find("div", class_="table_0")
+tag = tag.find("p", class_="title")
+tag = tag.get_text().strip()
+tag = tag.split("\t")
+tag = tag[-1]
+print(f"{tag}:\n")
+
+
+soup = soup.find("tbody")
+soup = soup.find_all("tr", class_="")
+# print(soup)
+for tr in soup:
+    row = tr.find_all("td")
+    name = row[0].get_text().strip()
+    curr = row[1].get_text().strip()
+    buyR = row[4].get_text().strip()
+    sellR = row[5].get_text().strip()
+    avgR = row[6].get_text().strip()
+    print(f"Currency: {curr} ({name})")
+    print(f"Buy rate: {buyR}")
+    print(f"Sell rate: {sellR}")
+    print(f"Average rate: {avgR}")
+    print("-" * 18)
+    
+
+
+
+
